7f90ddfb94d78f8373237ffcf6d186034fd084ca
Jordan Carlile <jordan.carlile@gmail.com>
2019-02-20 22:50:53 -0800

    now fetching todos via http get instead of storing locally

--- angular-crash-todolist/src/app/components/todo/todo.component.ts
+++ angular-crash-todolist/src/app/components/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service'
 import { Todo } from '../../models/Todo';
 
 @Component({
@@ -10,26 +11,12 @@ export class TodoComponent implements OnInit {
   todos:Todo[];
   name:string;
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
-    this.todos = [
-      {
-        id: 1,
-        title: "Todo One",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Todo Two",
-        completed: true
-      },
-      {
-        id: 3,
-        title: "Todo Three",
-        completed: false
-      }
-    ]
+    this.todoService.getTodos().subscribe(todos =>{
+      this.todos = todos;
+    });
   }
 
 }
